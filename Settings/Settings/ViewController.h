818b30446a560ecd62826b0b42fddc1529f599c6
yuMac.com <willing4567@163.com>
2015-09-17 17:53:20 +0800

    yuMac.com settings

---
+++ Settings/Settings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Settings
+//
+//  Created by yuMac on 15/9/17.
+//  Copyright (c) 2015年 yuMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Settings/Settings/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Settings
+//
+//  Created by yuMac on 15/9/17.
+//  Copyright (c) 2015年 yuMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
