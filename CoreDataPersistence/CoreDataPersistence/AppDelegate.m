56a584b5f175fb68583e8e64716941c275631f47
yuMac.com <willing4567@163.com>
2015-09-28 12:41:24 +0800

    yuMac.com coreData

--- CoreDataPersistence/CoreDataPersistence/AppDelegate.m
+++ CoreDataPersistence/CoreDataPersistence/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BIDViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    BIDViewController *control = [[BIDViewController alloc] init];
+    self.window.rootViewController = control;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- CoreDataPersistence/CoreDataPersistence/BIDViewController.m
+++ CoreDataPersistence/CoreDataPersistence/BIDViewController.m
@@ -8,6 +8,7 @@
 //
 
 #import "BIDViewController.h"
+#import "AppDelegate.h"
 
 @interface BIDViewController ()
 @property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *lineFields;
@@ -23,8 +24,26 @@ static NSString * const kLineTextKey = @"lineText";
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [delegate managedObjectContext];
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:kLineEntityName];
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request error:&error];
+    if (objects == nil) {
+        NSLog(@"Error occour : %@", error);
+    }
+    else {
+        for (NSManagedObject *oneObject in objects) {
+            NSInteger lineNum = [[oneObject valueForKey:kLineNumberKey] integerValue];
+            NSString *lineText = [oneObject valueForKey:kLineTextKey];
+            UITextField *textField = [self.lineFields objectAtIndex:lineNum];
+            if (textField != nil) {
+                [textField setText:lineText];
+            }
+        }
+    }
     
-    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:) name:UIApplicationWillResignActiveNotification object:[UIApplication sharedApplication]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,6 +51,38 @@ static NSString * const kLineTextKey = @"lineText";
     // Dispose of any resources that can be recreated.
 }
 
+- (void)applicationWillResignActive:(NSNotification *) notification {
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [delegate managedObjectContext];
+    for (int index = 0; index < 4; index ++) {
+        UITextField *textField = [self.lineFields objectAtIndex:index];
+        if (textField != nil) {
+            NSString *lineText = textField.text;
+            NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:kLineEntityName];
+            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(%K = %d)", kLineNumberKey, index];
+            [request setPredicate:predicate];
+            NSError *error;
+            NSArray *objects = [context executeFetchRequest:request error:&error];
+            if (objects == nil) {
+                NSLog(@"Error occour : %@", error);
+                return;
+            }
+            
+            NSManagedObject *oneObject;
+            if (objects.count > 0) {
+                oneObject = [objects objectAtIndex:0];
+            }
+            else {
+                oneObject = [NSEntityDescription insertNewObjectForEntityForName:kLineEntityName inManagedObjectContext:context];
+            }
+            [oneObject setValue:[NSNumber numberWithInt:index] forKey:kLineNumberKey];
+            [oneObject setValue:lineText forKey:kLineTextKey];
+        }
+    }
+    
+
+    [delegate saveContext];
+}
 /*
 #pragma mark - Navigation
 
