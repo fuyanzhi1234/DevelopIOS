251bca469fdb2990f957e161d1ee72b6a4d77f38
fuyanzhi1234 <fuyanzhi@iMac.local>
2016-05-24 22:41:09 +0800

    to afnetworking 3.0

--- TestAFNetworking/TestAFNetworking/NetWeatherManager.m
+++ TestAFNetworking/TestAFNetworking/NetWeatherManager.m
@@ -24,11 +24,21 @@ static NSString *kWeatherUrl = @"http://wthrcdn.etouch.cn/weather_mini?citykey=1
 }
 
 - (void)getWeatherInfo {
-    NSURL *url = [NSURL URLWithString:@"https://alpha-api.app.net/stream/0/posts/stream/global"];
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    /*AFNetworking 3.x写法*/
+    AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
+    sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript" , @"text/plain" , nil];
     
-    // 设置请求格式
-    //    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    [sessionManager GET:kWeatherUrl parameters:nil progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        [self parseWeatherJson:responseObject];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"Error:%@", error.description);
+        NSLog(@"AFJSONRequestSerializer Post Error");
+    }];
+    
+    /*
+    AFNetworking 2.x写法
+     
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     // 设置返回格式
     manager.responseSerializer = [AFJSONResponseSerializer serializer];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript" , @"text/plain" , nil];
@@ -39,6 +49,7 @@ static NSString *kWeatherUrl = @"http://wthrcdn.etouch.cn/weather_mini?citykey=1
         NSLog(@"Error:%@", error.description);
         NSLog(@"AFJSONRequestSerializer Post Error");
     }];
+    */
 }
 
 #pragma private
--- TestAFNetworking/TestAFNetworking/ViewController.m
+++ TestAFNetworking/TestAFNetworking/ViewController.m
@@ -44,21 +44,21 @@
 
 - (IBAction)sendHttp:(id)sender {
     
-    NSURL *url = [NSURL URLWithString:@"https://alpha-api.app.net/stream/0/posts/stream/global"];
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    
-    // 设置请求格式
-//    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
-    // 设置返回格式
-    manager.responseSerializer = [AFJSONResponseSerializer serializer];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript" , @"text/plain" , nil];
-
-    [manager GET:@"http://wthrcdn.etouch.cn/weather_mini?citykey=101190101" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject){
-        [self parseWeatherJson:responseObject];
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error:%@", error.description);
-        NSLog(@"AFJSONRequestSerializer Post Error");
-    }];
+//    NSURL *url = [NSURL URLWithString:@"https://alpha-api.app.net/stream/0/posts/stream/global"];
+//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    
+//    // 设置请求格式
+////    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+//    // 设置返回格式
+//    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript" , @"text/plain" , nil];
+//
+//    [manager GET:@"http://wthrcdn.etouch.cn/weather_mini?citykey=101190101" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject){
+//        [self parseWeatherJson:responseObject];
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Error:%@", error.description);
+//        NSLog(@"AFJSONRequestSerializer Post Error");
+//    }];
 }
 
 #pragma private
